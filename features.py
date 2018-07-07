bb8270d6ccca0dbf7ead2ecf98b86460610645c5
Preetam2114 <preetamrane2114@gmail.com>
2018-07-07 11:03:10 +0530

    First commit

---
+++ features.py
@@ -0,0 +1,46 @@
+import mahotas
+import cv2
+
+
+#fix the size of image
+fixed_size = tuple((100, 100))
+
+# path to training data
+train_path = "dataset/training_set/"
+
+# no.of.trees for Random Forests
+num_trees = 100
+
+# bins for histogram
+bins = 8
+
+# train_test_split size
+test_size = 0.10
+
+# seed for reproducing same results
+seed = 9
+
+def fd_hu_moments(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    feature = cv2.HuMoments(cv2.moments(image)).flatten()
+    return feature
+
+# feature-descriptor-2: Haralick Texture
+def fd_haralick(image):
+    # convert the image to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # compute the haralick texture feature vector
+    haralick = mahotas.features.haralick(gray).mean(axis=0)
+    # return the result
+    return haralick
+
+# feature-descriptor-3: Color Histogram
+def fd_histogram(image, mask=None):
+    # convert the image to HSV color-space
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    # compute the color histogram
+    hist  = cv2.calcHist([image], [0, 1, 2], None, [bins, bins, bins], [0, 256, 0, 256, 0, 256])
+    # normalize the histogram
+    cv2.normalize(hist, hist)
+    # return the histogram
+    return hist.flatten()
